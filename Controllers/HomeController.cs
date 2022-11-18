3ceea3ecd519dd0b55e202d20f4bff7a45dc3da4
RunaliPatel27 <rpate629@my.centennialcollege.ca>
2022-11-18 18:43:15 -0500

    Added Controller & View

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace RazorPagesMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+
+
+   
