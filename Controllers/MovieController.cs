3ed079cae117ca46364ac7aae186867270e8a67f
RunaliPatel27 <rpate629@my.centennialcollege.ca>
2022-11-18 21:36:02 -0500

    Added Search and validation

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    public async Task<IActionResult> Index(string searchString)
+    {
+        var movies = from m in _context.Movie
+                     select m;
+
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.Contains(searchString));
+        }
+
+        return View(await movies.ToListAsync());
+    }
+}
